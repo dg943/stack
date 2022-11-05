91c7b9b8594c73005a98cd0f2fea09d9a8cb87e0
Dinanath Gupta <Dinanath.gupta@dpworld.com>
2022-11-05 12:06:26 +0530

    stack

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/dg943/stack
+
+go 1.18
---
+++ stack.go
@@ -0,0 +1,46 @@
+package stack
+
+import "fmt"
+
+func NewStack[T any]() *Stack[T] {
+	return &Stack[T]{}
+}
+
+type Stack[T any] struct {
+	arr []T
+}
+
+func (s *Stack[T]) String() string {
+	string_s := "["
+	for _, e := range s.arr {
+		string_s = fmt.Sprintf("%s%#v, ", string_s, e)
+	}
+	if string_s == "[" {
+		return ""
+	}
+	string_s = string_s[:len(string_s)-2]
+	string_s = string_s + "]"
+	return string_s
+}
+
+func (s *Stack[T]) push(e ...T) {
+	s.arr = append(s.arr, e...)
+}
+
+func (s *Stack[T]) pop() (T, bool) {
+	if len(s.arr) < 1 {
+		var noop T
+		return noop, false
+	}
+	top := s.arr[len(s.arr)-1]
+	s.arr = s.arr[:len(s.arr)-1]
+	return top, true
+}
+
+func (s *Stack[T]) top() (T, bool) {
+	if len(s.arr) < 1 {
+		var noop T
+		return noop, false
+	}
+	return s.arr[len(s.arr)-1], true
+}
